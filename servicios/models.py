a4d1085e043cb8ffb82c3a7b087a003e51a81ccb
Diego Ponce <daikoponce@gmail.com>
2026-08-25 10:51:17 -0300

    Feature: CRUD y HTML de Servicio (#3)
    * Inicializado Servicios
    
    * feat:CRUD de Servicio + HTML (#2)

--- servicios/models.py
+++ servicios/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Servicios(models.Model):
-    pass
+class Servicio(models.Model):
+    nombre = models.CharField('Nombre', max_length=100)
+    descripcion = models.TextField('Descripcion')
+    precio = models.DecimalField('Precio', max_digits=10, decimal_places=2)
+    activo = models.BooleanField('Activo', default=True)
+
+    class Meta:
+        verbose_name = 'Servicio'
+        verbose_name_plural = 'Servicios'
+
+    def __str__(self):
+        return self.nombre
 
 class Clientes(models.Model):
     pass
--- servicios/views.py
+++ servicios/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, View
+from .models import Servicio
 
-# Create your views here.
+
+# Views de listas
+
+# Listar solo los activos
+class ServicioListView(ListView):
+    model = Servicio
+    template_name = 'servicios/listar_servicios.html'
+    context_object_name = 'servicios'
+    def get_queryset(self):
+        return Servicio.objects.filter(activo=True)
+
+# Listar inactivos
+class ServicioInactivosListView(ListView):
+    model = Servicio
+    template_name = 'servicios/inactivos.html'
+    context_object_name = 'servicios'
+
+    def get_queryset(self):
+        return Servicio.objects.filter(activo=False)
+
+
+# Views de creación, edición, eliminación y restauración
+
+#Creación
+class ServicioCreateView(CreateView):
+    model = Servicio
+    template_name = 'servicios/form.html'
+    fields = ['nombre', 'descripcion', 'precio']
+    success_url = reverse_lazy('servicios:listar')
+
+#Edición
+class ServicioUpdateView(UpdateView):
+    model = Servicio
+    fields = ['nombre', 'descripcion', 'precio', 'activo']
+    template_name = 'servicios/form.html'
+    success_url = reverse_lazy('servicios:listar')
+
+#Baja Lógica
+class ServicioBajaLogicaView(View):
+    def post(self, request, pk):
+        servicio = get_object_or_404(Servicio, pk=pk)
+        servicio.activo = False
+        servicio.save()
+        return redirect('servicios:listar')
+
+#Restauración (active = False -> True)
+class ServicioRestaurarView(View):
+    def post(self, request, pk):
+        servicio = get_object_or_404(Servicio, pk=pk)
+        servicio.activo = True
+        servicio.save()
+        return redirect('servicios:inactivos')
