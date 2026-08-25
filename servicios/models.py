e57fd533682f018328d4a301f80462d7d2c5c16f
Daiko <daikoponce@gmail.com>
2026-08-24 21:46:04 -0300

    Inicializado Servicios

--- servicios/models.py
+++ servicios/models.py
@@ -2,7 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Servicios(models.Model):
-    pass
+    nombre = models.CharField('Nombre', max_length=100)
+    descripcion = models.TextField('Descripcion')
+    precio = models.DecimalField('Precio', max_digits=10, decimal_places=2)
+    activo = models.BooleanField('Activo', default=True)
+    creado_el = models.DateTimeField('Fecha de creacion', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Servicio'
+        verbose_name_plural = 'Servicios'
+
+    def __str__(self):
+        return self.nombre
 
 class Clientes(models.Model):
     pass
