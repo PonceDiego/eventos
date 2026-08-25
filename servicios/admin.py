bdf0dc9b68aee01a1ee2164fc29d71b20f109bfd
Daiko <daikoponce@gmail.com>
2026-08-25 12:08:08 -0300

    feat: DjangoAdmin Servicio

--- servicios/admin.py
+++ servicios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Servicio
 
 # Register your models here.
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'activo')
+    search_fields = ('nombre',)
+    list_filter = ('activo',)
