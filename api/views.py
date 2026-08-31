b289f8eae95357dc1f775b4a7370b2a68119a86c
Daiko <daikoponce@gmail.com>
2026-08-31 14:44:37 -0300

    fix: Diferenciación entre JSON de detalle y lista + url detalle

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
 from servicios.models import Servicio, Cliente, Coordinador , Empleado
 from .serializers import ServicioSerializer, ClienteSerializer , CoordinadorSerializer ,EmpleadoSerializer
-from django.shortcuts import  get_object_or_404
 
 
 # Create your views here.
@@ -10,50 +10,87 @@ from django.shortcuts import  get_object_or_404
 @api_view(['GET'])
 def lista_servicios(request):
     servicios = Servicio.objects.all()
-    serializer = ServicioSerializer(servicios , many=True)
+    serializer = ServicioSerializer(servicios , many=True, context={'request' : request})
     return Response(serializer.data)
 
 @api_view(['GET'])
 def detalle_servicio(request,servicio_id):
-    servicio=get_object_or_404(Servicio , pk = servicio_id)
-    serializer= ServicioSerializer(servicio)
+    try:
+        servicio = Servicio.objects.get(pk=servicio_id)
+    except Servicio.DoesNotExist:
+        return Response(
+            {"razón": f"No existe un servicio asociado al id número {servicio_id}."}, 
+            status=status.HTTP_404_NOT_FOUND
+        )
+    sin_detalle = ['id', 'nombre', 'descripcion', 'precio', 'activo']
+    serializer= ServicioSerializer(servicio,
+        fields = sin_detalle,
+        context={'request' : request})
     return Response(serializer.data)
 
 #CLIENTE
 @api_view(['GET'])
 def lista_clientes(request):
     clientes = Cliente.objects.all()
-    serializer= ClienteSerializer(clientes , many=True)
+    serializer= ClienteSerializer(clientes , many=True, context={'request' : request})
     return Response(serializer.data)
 
 @api_view(['GET'])
 def detalle_cliente(request,cliente_id):
-    cliente = get_object_or_404(Cliente, pk=cliente_id)
-    serializer= ClienteSerializer(cliente)
+    try:
+        cliente = Cliente.objects.get(pk=cliente_id)
+    except Cliente.DoesNotExist:
+        return Response(
+            {"razón": f"No existe un cliente asociado al id número {cliente_id}."},
+            status = status.HTTP_404_NOT_FOUND
+        )
+    sin_detalle = ['id', 'nombre', 'apellido', 'contacto', 'activo']
+    serializer= ClienteSerializer(cliente,
+        fields = sin_detalle,
+        context={'request' : request})
     return Response(serializer.data)
 
 #COORDINADOR
 @api_view(['GET'])
 def lista_coordinadores(request):
     coordinadores= Coordinador.objects.all()
-    serializer = CoordinadorSerializer(coordinadores , many=True)
+    serializer = CoordinadorSerializer(coordinadores , many=True, context={'request' : request})
     return  Response(serializer.data)
 
 @api_view(['GET'])
 def detalle_coordinador(request, coordinador_id):
-    coordinador = get_object_or_404(Coordinador , pk = coordinador_id)
-    serializer = CoordinadorSerializer(coordinador)
+    try:
+
+        coordinador = Coordinador.objects.get(pk = coordinador_id)
+    except Coordinador.DoesNotExist:
+        return Response(
+            {"razón": f"No existe un coordinador asociado al id número {coordinador_id}."},
+            status = status.HTTP_404_NOT_FOUND
+        )
+    sin_detalle = ['id', 'nombre', 'apellido', 'dni', 'fecha_alta', 'activo']
+    serializer= CoordinadorSerializer(coordinador,
+            fields = sin_detalle,
+            context={'request' : request})
     return Response(serializer.data)
 
 #EMPLEADO
 @api_view(['GET'])
 def lista_empleados(request):
     empleados= Empleado.objects.all()
-    serializer= EmpleadoSerializer(empleados , many=True)
+    serializer= EmpleadoSerializer(empleados , many=True, context={'request' : request})
     return Response(serializer.data)
 
 @api_view(['GET'])
 def detalle_empleado(request,empleado_id):
-    empleado=get_object_or_404(Empleado, pk = empleado_id)
-    serializer = EmpleadoSerializer(empleado)
+    try:
+        empleado=Empleado.objects.get(pk = empleado_id)
+    except Empleado.DoesNotExist:
+        return Response(
+            {"razón": f"No existe un empleado asociado al id número {empleado_id}."},
+            status = status.HTTP_404_NOT_FOUND
+        )
+    sin_detalle = ['id', 'nombre', 'apellido', 'legajo', 'activo']
+    serializer= EmpleadoSerializer(empleado,
+        fields = sin_detalle,
+        context={'request' : request})
     return Response(serializer.data)
